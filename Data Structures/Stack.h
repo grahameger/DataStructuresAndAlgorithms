40156c1f7162cf68f3e7e4bdbf5130e3b632f133
eger <eger@umich.edu>
2017-05-29 22:22:47 -0400

    add Stack

---
+++ Data Structures/Stack.h
@@ -0,0 +1,51 @@
+/*
+ * Stack data struture implemented
+ * using a std::vector
+ *
+ * Graham Eger
+ * 
+ *
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <iostream>
+#include <cassert>
+
+template <typename T>
+class Stack {
+public:
+	
+	void reserve(size_t n) {
+		v.reserve(v);
+	}
+	
+	void push(const T &datum) {
+		v.push_back(datum);
+	}
+	
+	void pop() {
+		assert(v.size() != 0, "Tried to pop a stack of size 0\n");
+		v.pop_back();
+	}
+	
+	T& top() {
+		assert(v.size() != 0, "Tried to get top of a stack of size 0\n");
+		return v.back();
+	}
+
+	size_t size() {
+		return v.size();
+	}
+
+	bool empty() {
+		return !v.size();
+	}
+	
+private:
+	std::vector<T> v;
+};
+
+
+#endif
