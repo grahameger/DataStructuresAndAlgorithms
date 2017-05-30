207123852fc7eb964775b566f1ef80dc2994810c
Graham Eger <eger@umich.edu>
2017-05-30 09:34:58 -0400

    add Streaming Median, start Binary Search tree

---
+++ Data Structures/BinarySearchTree.h
@@ -0,0 +1,79 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+/*
+ * Binary search tree data structure
+ * Templated
+ *
+ *
+ */
+
+
+#include <cassert>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+
+template<typename T, typename COMPARE = std::less<T> >
+class BinarySearchTree
+{
+public:
+	BinarySearchTree() {
+		root = nullptr;
+	}
+
+	// Copy constructor
+	// TODO
+
+	// Assignement operator
+	// TODO
+
+	// Destructor
+	~BinarySearchTree() {
+		clear(root);
+	}
+
+	// O(n)
+	size_t size(Node * node) const {
+		if (!node) {
+			return 0;
+		} else {
+			return size(node->left) + size(node->right) + 1;
+		}
+	}
+
+	bool empty() const {
+		return !size;
+	}
+
+
+private:
+
+	// Deletes all nodes below node
+	void clear(Node * node) {
+		if (node != nullptr) {
+			clear(node->left);
+			clear(node->right);
+			delete node; node = nullptr;
+		}
+	}
+
+	struct Node
+	{
+		Node(const T &datum_in, Node * left_in, Node * right_in) {
+			datum = datum_in;
+			left = left_in;
+			right = right_in;
+		}
+
+		T datum;
+		Node * left;
+		Node * right;
+	};
+
+	Node * root;
+	COMPARE less;
+	size_t size;
+
+
+};
---
+++ Data Structures/StreamingMedian.h
@@ -0,0 +1,54 @@
+#ifndef MEDIAN_H
+#define MEDIAN_H
+
+// A templated streaming median class, push values and grab the median at any point
+// uses std::less and std::greater
+
+#include <iostream>
+#include <queue>
+#include <limits>
+
+
+// Type must me a numeric type
+template <typename T>
+class StreamingMedian {
+public:
+	// Seed the queues
+	StreamingMedian() {
+		upper.push(std::numeric_limits<T>::max());
+		lower.push(std::numeric_limits<T>::min());
+	}
+
+	void push(const T &val) {
+		if (val >= upper.top()) {
+			upper.push(val);
+		} else {
+			lower.push(val);
+		}
+
+		// rebalance if necessary
+		// upper queue is larger
+		if (upper.size() - lower.size() == 2) {
+			lower.push(upper.top);
+			upper.pop();
+		} else if (lower.size() - upper.size() == 2) {
+			upper.push(lower.top()) 
+			lower.pop();
+		}
+	}
+
+	T& median() {
+		if (upper.size() == lower.size()) {
+			return (upper.top() + lower.top()) / 2.0;
+		} else if (upper.size() > lower.size()) {
+			return upper.top();
+		} else {
+			return lower.top();
+		}
+	}
+private:
+	std::priority_queue<T, std::greater<T> > upper;
+	std::priority_queue<T, std::less<T> >    lower;
+};
+
+#endif
